0e83c6ca24db62e6855fedd0bd9eea6bca3210a5
Richard Basdeo <basdeorichard@gmail.com>
2021-02-10 13:10:27 -0500

    Initial Commit

---
+++ Movie Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Flix
+//
+//  Created by Richard Basdeo on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
